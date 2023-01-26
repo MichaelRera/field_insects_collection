f615838a0e63915b6600029c6433e7d918feaf7d
Michael Rera <michael_rera@yahoo.fr>
2023-01-26 11:46:48 +0100

    simulated model2 compet to Smurfs

--- figures/pop_sims/pop_sim_skaakai/script_model2.sh
+++ figures/pop_sims/pop_sim_skaakai/script_model2.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-for birth_rate in 0.1 
+for birth_rate in  0.1 0.2 0.3 0.4  
 do
-	for compet in 1e-6
+	for compet in 1e-6 1e-5 1e-4
 	do
-		for compet2 in 1e-7
-		Rscript DiMeglio_Model2_exponential_transition_immigration_compet2smurf.R $birth_rate $compet $compet2 &
-		echo 'Performing simulation with parameters:' $birth_rate $compet $compet2 "finished" &
+		for compet2 in 1e-7 1e-6 
+		do
+			Rscript DiMeglio_Model2_exponential_transition_immigration_compet2smurf.R $birth_rate $compet $compet2 &
+			echo 'Performing simulation with parameters:' $birth_rate $compet $compet2 "finished" &
+		done
 	done
 done
